ea66144a533d35cdfa03d5f5b258d766cee7238a
Phil Jirsa <phil.jirsa@microsoft.com>
2024-05-06 11:20:05 -0500

    add odata openapi extensions

---
+++ dataapi/Extensions/ODataOpenApiBuilderExtensions.cs
@@ -0,0 +1,48 @@
+﻿//-----------------------------------------------------------------------------
+// <copyright file="ODataOpenApiBuilderExtensions.cs" company=".NET Foundation">
+//      Copyright (c) .NET Foundation and Contributors. All rights reserved.
+//      See License.txt in the project root for license information.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using Microsoft.AspNetCore.Builder;
+
+namespace DataApi;
+
+/// <summary>
+/// Provides extension methods for <see cref="IApplicationBuilder"/> to add OData routes.
+/// </summary>
+public static class ODataOpenApiBuilderExtensions
+{
+    /// <summary>
+    /// Use OData OpenApi middleware.
+    /// </summary>
+    /// <param name="app">The <see cref="IApplicationBuilder "/> to use.</param>
+    /// <returns>The <see cref="IApplicationBuilder "/>.</returns>
+    public static IApplicationBuilder UseODataOpenApi(this IApplicationBuilder app)
+    {
+        return app.UseODataOpenApi(route: "$openapi");
+    }
+
+    /// <summary>
+    /// Use OData OpenApi middleware.
+    /// </summary>
+    /// <param name="app">The <see cref="IApplicationBuilder "/> to use.</param>
+    /// <param name="route">The route name used to query the openapi.</param>
+    /// <returns>The <see cref="IApplicationBuilder "/>.</returns>
+    public static IApplicationBuilder UseODataOpenApi(this IApplicationBuilder app, string route)
+    {
+        if (app == null)
+        {
+            throw new ArgumentNullException(nameof(app));
+        }
+
+        if (string.IsNullOrWhiteSpace(route))
+        {
+            throw new ArgumentNullException(nameof(route));
+        }
+
+        return app.UseMiddleware<ODataOpenApiMiddleware>(route);
+    }
+}
---
+++ dataapi/Extensions/ODataOpenApiPathProvider.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------------
+// <copyright file="ODataOpenApiPathProvider.cs" company=".NET Foundation">
+//      Copyright (c) .NET Foundation and Contributors. All rights reserved.
+//      See License.txt in the project root for license information.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using Microsoft.OData.Edm;
+using Microsoft.OpenApi.OData;
+using Microsoft.OpenApi.OData.Edm;
+
+namespace DataApi;
+
+/// <summary>
+/// OData openapi path provider
+/// </summary>
+internal class ODataOpenApiPathProvider : IODataPathProvider
+{
+    private IList<ODataPath> _paths = new List<ODataPath>();
+
+    public bool CanFilter(IEdmElement element)
+    {
+        return true;
+    }
+
+    public IEnumerable<ODataPath> GetPaths(IEdmModel model, OpenApiConvertSettings settings)
+    {
+        return this._paths;
+    }
+
+    public void Add(ODataPath path)
+    {
+        this._paths.Add(path);
+    }
+}
