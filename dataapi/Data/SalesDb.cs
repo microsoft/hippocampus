ca146fc9ba7005b6d6ca3680f59063766b569290
Phil Jirsa <phil.jirsa@microsoft.com>
2024-05-07 14:13:54 -0500

    add sql db backend to dataapi

---
+++ dataapi/Data/SalesDb.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+using Microsoft.EntityFrameworkCore;
+using DataApi.Models;
+
+namespace DataApi.Data;
+
+public class SalesDb : DbContext
+{
+    public SalesDb(DbContextOptions<SalesDb> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Asset>().ToTable("Assets");
+        modelBuilder.Entity<Order>().ToTable("Sales");
+    }
+
+    public DbSet<Asset> Assets { get; set; }
+
+    public DbSet<Order> Sales { get; set; }
+}
--- dataapi/Models/SingletonDatastore.cs
+++ dataapi/Models/SingletonDatastore.cs
@@ -1,6 +1,4 @@
-﻿using DataApi.Helpers;
-
-namespace DataApi.Models;
+﻿namespace DataApi.Models;
 
 public class SingletonDatastore
 {
@@ -10,8 +8,8 @@ public class SingletonDatastore
     public SingletonDatastore()
     {
         DataApi.Helpers.CSVReader<AssetsMap> _assetReader = new();
-        DataApi.Helpers.CSVReader<AssetsMap> _orderReader = new();
-        this.Assets = _assetReader.ReadCSV<Asset>("Files/neuro_assets_sample.csv");
-        this.Orders = _orderReader.ReadCSV<Order>("Files/neuro_sales_unique.csv");
+        DataApi.Helpers.CSVReader<OrdersMap> _orderReader = new();
+        this.Assets = _assetReader.ReadCSV<Asset>("Files/neuro_assets_SMALL.csv");
+        this.Orders = _orderReader.ReadCSV<Order>("Files/neuro_sales_SMALL.csv");
     }
 }
