f8f546a57f4a2dc2df4515e8be6687f480aaf599
Kyle Raymond <117292869+skraymo1@users.noreply.github.com>
2024-05-08 15:19:28 -0500

    Revert "Dataapideployment"

--- dataapi/helpers/CSVReaderHelper.cs
+++ dataapi/helpers/CSVReaderHelper.cs
@@ -1,19 +1,19 @@
 ﻿using CsvHelper;
-using DataApi.Models;
+using CsvHelper.Configuration;
 using System.Globalization;
+
 namespace DataApi.Helpers;
 
-
-public class CSVReader
+public class CSVReader<TMap> where TMap : ClassMap
 {
-    public List<Asset> ReadCSV(string filePath)
+    public List<T> ReadCSV<T>(string filePath)
     {
         using (var reader = new StreamReader(filePath))
         using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
         {
-            csv.Context.RegisterClassMap<AssetsMap>();
-            var records = csv.GetRecords<Asset>();
-            return new List<Asset>(records);
+            csv.Context.RegisterClassMap<TMap>();
+            var records = csv.GetRecords<T>();
+            return new List<T>(records);
         }
     }
 }
