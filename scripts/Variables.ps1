594008786fe8648fb6950f9be98f88f1e4099d94
Phil Jirsa <phil.jirsa@microsoft.com>
2024-05-10 09:35:42 -0500

    revert model name change

--- scripts/Variables.ps1
+++ scripts/Variables.ps1
@@ -2,7 +2,7 @@
 
 # Default values
 $varCompletionModelOpenAI = "gpt-3.5-turbo"
-$varCompletionModelAzureOpenAI = "gpt-35-turbo-16k"
+$varCompletionModelAzureOpenAI = "gpt-35-turbo"
 $varEmbeddingModel = "text-embedding-ada-002"
 $varASPNetCore = "Development"
 $varInstance = "https://login.microsoftonline.com"
